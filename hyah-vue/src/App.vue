255b68fb8b2684db8defd3f2a96273bcfecae813
Nikki Bright <nikkistorme@gmail.com>
2023-06-20 11:57:28 -0500

    feat: :sparkles: 1.1.0: ProgressBar component

--- hyah-vue/src/App.vue
+++ hyah-vue/src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import Button from "./components/Button/HButton.vue";
+import Button from "./components/Button";
+import ProgressBar from "./components/ProgressBar/ProgressBar.vue";
 </script>
 
 <template>
@@ -8,5 +9,7 @@ import Button from "./components/Button/HButton.vue";
     <h2>Vue</h2>
     <h3>Button</h3>
     <Button>Click me</Button>
+    <h3>Progress</h3>
+    <ProgressBar />
   </div>
 </template>
